cf1b9b3451965ff48267c44baf70fe3262bca1a2
KNajman <karel.najman@tul.cz>
2021-03-25 10:40:49 +0100

    Update kodu

--- Marathon/src/marathon/Runner.java
+++ Marathon/src/marathon/Runner.java
@@ -1,5 +1,7 @@
 package marathon;
 
+import java.time.LocalTime;
+
 /**
  *
  * @author najma
@@ -7,10 +9,14 @@ package marathon;
 public class Runner {
 
     private final int number;
+    private LocalTime startTime;
+    private LocalTime finishTime;    
+    
     private int HH;
     private int MM;
     private int SS;
 
+    //construktor
     public Runner(int number, int HH, int MM, int SS) {
         this.number = number;
         this.HH = HH;
@@ -24,15 +30,44 @@ public class Runner {
 
     }
 
+    public int getTimeInSeconds() {
+        return 3600 * this.HH + 60 * this.MM + this.SS;
+    }
+    
+    
+    
+
     @Override
     public String toString() {
         return "Běžec číslo: " + number + " s cilovy casem = " + HH + ":" + MM + ":" + SS;
     }
-    
-    
+
 //    public static void main(String[] args) {
 //        Runner Pavel = new Runner(1001,11,16,39);
 //        System.out.println(Pavel);
 //    }
+    public int getHH() {
+        return HH;
+    }
+
+    public void setHH(int HH) {
+        this.HH = HH;
+    }
+
+    public int getMM() {
+        return MM;
+    }
+
+    public void setMM(int MM) {
+        this.MM = MM;
+    }
+
+    public int getSS() {
+        return SS;
+    }
+
+    public void setSS(int SS) {
+        this.SS = SS;
+    }
 
 }
